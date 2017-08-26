d6b8c8698666d88ce22991cd5cf4874dce4c1d46
Jason Khanlar <jason.khanlar@gmail.com>
2017-08-26 13:31:45 -0400

    Add deobfuscation for window[client][try_ping]

--- starve.io-deobfuscate.user.js
+++ starve.io-deobfuscate.user.js
@@ -1008,6 +1008,9 @@
                             } else if (deobfunc.abbr.match(/^function \(\){if\(@-this\.@>@\.@\){this\.@=@;var [a-z]=user\.cam,[a-z]=Math.floor\([a-z]\.x\/100\),[a-z]=Math\.floor\([a-z]\.y\/100\);/)) {
                                 window.update_cam = s;
                                 deobmatch('update_cam', s);
+                            } else if (deobfunc.abbr.match(/^function \(\){@-this\.@>@\.PING_DELAY&&\(this\.@=@,this\.ping\(\)\)}$/)) {
+                                window.try_ping = s;
+                                deobmatch('try_ping', s);
                             }
                         } else if (window[client][s].length === 1) {
                             if (deobfunc.abbr.match(/user\.inv\.max==user\.inv\.can_select\.length/)) {
