51e246fd510ed1b228645588fcc0dc801b344d19
Jason Khanlar <jason.khanlar@gmail.com>
2017-08-28 17:18:39 -0400

    Draw shop countdown timer only if shop button is active, and also not in spectator mode

--- starve.io-extensions.user.js
+++ starve.io-extensions.user.js
@@ -410,7 +410,7 @@
         window.old_user_shop_draw = user.shop.draw;
         user.shop.draw = function() {
             old_user_shop_draw.apply(this);
-            if (60 > this.time) draw_shop_timer();
+            if (!user.spectator && 60 > this.time && this.time > 0) draw_shop_timer();
         };
 
         window.old_user_control_update = user.control.update;
