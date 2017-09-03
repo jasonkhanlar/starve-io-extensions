4e629938af4604ff5282992047444dc64268eedb
Jason Khanlar <jason.khanlar@gmail.com>
2017-09-03 09:56:30 -0400

    Add deobfuscation for draw_gauges

--- starve.io-deobfuscate.user.js
+++ starve.io-deobfuscate.user.js
@@ -119,6 +119,9 @@
                         } else if (deobfunc.abbr.match(/var [a-z]=user\.inv,/)) { // v15 1 match
                             window.draw_ui_inventory = s;
                             deobmatch('draw_ui_inventory', s);
+                        } else if (deobfunc.abbr.match(/user\.gauges\.warn_life/)) { // v15 1 match
+                            window.draw_gauges = s;
+                            deobmatch('draw_gauges', s);
                         }
                     } else if (window[s].length === 1) { // v15 51 matches
                         if (deobfunc.abbr.match(/[a-z]=-[a-z]\.width;/) && deobfunc.abbr.match(/ROTATE;if\(this\.hit\.update\)/)) {
