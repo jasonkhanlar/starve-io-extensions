9574e085ef68336994f62c2aa80733e03a9ba430
Jason Khanlar <jason.khanlar@gmail.com>
2017-09-03 10:17:00 -0400

    Add deobfuscation for draw_furnace

--- starve.io-deobfuscate.user.js
+++ starve.io-deobfuscate.user.js
@@ -131,6 +131,9 @@
                         } else if (s !== 'create_images' && deobfunc.orig.match(/DRIED_PLANT/)) { // v15 1 match
                             window.draw_plant = s;
                             deobmatch('draw_plant', s);
+                        } else if (s !== 'create_images' && deobfunc.orig.match(/FURNACE_ON/)) { // v15 1 match
+                            window.draw_furnace = s;
+                            deobmatch('draw_furnace', s);
                         }
                     } else if (window[s].length === 1) { // v15 51 matches
                         if (deobfunc.abbr.match(/[a-z]=-[a-z]\.width;/) && deobfunc.abbr.match(/ROTATE;if\(this\.hit\.update\)/)) {
