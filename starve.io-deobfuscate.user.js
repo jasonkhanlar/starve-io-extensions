f359d4f0612c4fa4fe54f98f84be34920a0fc51e
Jason Khanlar <jason.khanlar@gmail.com>
2017-09-10 00:44:10 -0400

    Update version, now compatible with Mozilla Firefox Greasemonkey and Tampermonkey

--- starve.io-deobfuscate.user.js
+++ starve.io-deobfuscate.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Starve.io Deobfuscated Auto
 // @namespace    http://tampermonkey.net/
-// @version      0.15.30
+// @version      0.15.50
 // @description  Auto deobfuscation includes at least bare minimum for scripts to function normally
 // @author       Jason Khanlar
 // @match        http://starve.io/
