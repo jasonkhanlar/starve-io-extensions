81a6d66a33f4b3b3d0859cd20b8d2444c46296c9
Jason Khanlar <jason.khanlar@gmail.com>
2017-09-18 00:53:38 -0400

    update version

--- starve.io-extensions.user.js
+++ starve.io-extensions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Starve.io extensions
 // @namespace    http://tampermonkey.net/
-// @version      0.15.52
+// @version      0.15.53
 // @description  (1) On screen chat buffer (2) On screen help (3) Auto attack (4) Auto book (5) Auto cook (6) Auto follow (7) Copy craft (8) Active feed (9) Server name (10) Gauge values (11) GPS
 // @author       Jason Khanlar
 // @match        http://starve.io/
